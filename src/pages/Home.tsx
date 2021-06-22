698fbcd5ba46e8d1e0ea02a0c200f18e81e1d667
unknown <leonardo.costa@EXT-ENG26.exsto.local>
2021-06-21 22:06:30 -0300

    feat: finish day02

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,43 @@
-import { useHistory } from 'react-router-dom'
-import IllustrationImg from '../assets/images/illustration.svg';
-import logoImg from "../assets/images/logo.svg";
-import googleImg from "../assets/images/google-icon.svg";
+import { useHistory } from 'react-router-dom';
+
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
+
+import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
-import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 export function Home() {
   const history = useHistory()
+  const { user, signInWithGoogle } = useAuth()
 
-  function navigateToNewRoom() {
-    history.push('rooms/new');
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
+    }
+
+    history.push('/rooms/new')
   }
+
   return (
     <div id="page-auth">
       <aside>
-        <img src={IllustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>
           Tire as dúvidas da sua audiência em tempo-real
-      </p>
+        </p>
       </aside>
 
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button onClick={navigateToNewRoom} className="create-room">
-            <img src={googleImg} alt="Logo do Google" />
-          Crie sua sala com o Google
-        </button>
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
 
           <div className="separator">ou entre em uma sala</div>
 
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app'
+import firebase from 'firebase/app';
 
 import 'firebase/auth';
 import 'firebase/database';
@@ -8,12 +8,15 @@ const firebaseConfig = {
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCK,
-    messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID
 };
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
-const auth = firebase.auth();
-export const database = firebase.database();
+const auth = firebase.auth()
+const database = firebase.database()
+
+
+export { firebase, auth, database };
